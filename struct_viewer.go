514ef922f6a45f0d9cc26575acddf70913361fe9
tbuchaillot <tombuchaillot89@gmail.com>
2022-08-05 13:46:09 +0200

    adding parser

---
+++ struct_viewer.go
@@ -0,0 +1,19 @@
+package struct_viewer
+
+type Viewer struct {
+	config interface{}
+	prefix string
+
+	envs []EnvVars
+}
+
+func New(config interface{}, prefix string) *Viewer {
+	cfg := Viewer{config: config, prefix: prefix}
+	cfg.Start()
+	return &cfg
+}
+
+func (h *Viewer) Start() {
+	h.envs = parseEnvs(h.config)
+}
+
