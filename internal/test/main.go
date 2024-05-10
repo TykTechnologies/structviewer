ae27572e37e1dcab0046865128e1b49b556c010a
Matias <83959431+mativm02@users.noreply.github.com>
2024-05-10 11:42:56 -0300

    removing unused struct

--- internal/test/main.go
+++ internal/test/main.go
@@ -7,43 +7,6 @@ import (
 	"github.com/TykTechnologies/structviewer"
 )
 
-// InnerStructType represents an inner struct.
-type InnerStructType struct {
-	// DummyAddr represents an address.
-	DummyAddr string `json:"dummy_addr"`
-}
-
-// StructType represents a struct type.
-type StructType struct {
-	// Enable represents status.
-	Enable bool `json:"enable"`
-	// Inner is an inner struct.
-	Inner InnerStructType `json:"inner"`
-}
-
-type testStruct struct {
-	// Exported represents a sample exported field.
-	Exported    string `json:"exported"`
-	notExported bool   //lint:ignore U1000 Unused field is used for testing purposes.
-
-	// StrField is a struct field.
-	StrField struct {
-		// Test is a field of struct type.
-		Test  string `json:"test"`
-		Other struct {
-			// OtherTest represents a field of sub-struct.
-			OtherTest   bool `json:"other_test"`
-			nonEmbedded string
-		} `json:"other"`
-	} `json:"str_field"`
-
-	// ST is another struct type.
-	ST StructType `json:"st"`
-
-	// JSONExported includes a JSON tag.
-	JSONExported int `json:"json_exported" structviewer:"obfuscate"`
-}
-
 type complexType struct {
 	Data struct {
 		Object1 int  `json:"object_1,omitempty"`
